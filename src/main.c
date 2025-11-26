619d21cc298575ed233983b08d466056eadf2df3
Nestor Neto <nestor-dp@hotmail.com>
2025-11-26 11:32:51 -0300

    Fix comments style

--- src/main.c
+++ src/main.c
@@ -16,44 +16,69 @@
 
 int main(void)
 {
-	unsigned int contTec = 0;
-  unsigned int contTecRapido = 0;
+	unsigned int cont_tec = 0;
+  unsigned int cont_tec_rapido = 0;
 
 	configPeripherals();	
 	convertVariable(var);
 	
 	while (1)
 	{ 
-		if (bit_is_clear(PIND, _chave01))					//Testa se a chave 01 foi precionada (ch++)			
+    //Testa se a chave 01 foi precionada (ch++)	
+		if (bit_is_clear(PIND, _chave01))							
 		{
-			_delay_ms(20);									//espera passar o bounce 
-			var++;											//incrementa variável do display
-			if_value_max(99);								//atribue um valor máximo para a variável
-			convertVariable(var);							//quebra a variável em unit e dozen para o display 7 seg
-			while(bit_is_clear(PIND, _chave01))				//enquanto a chave tiver precionada incrementa a variável
-			{
-				if(contTecRapido < NUM_VEZES_LENTO){		//testa quantas vezes a var foi incrementada sem ser solta
-					if(contTec >= TEMPO_INCREMENTO_LENTO){	//testa se já passo o tempo para incrmento lento
+      //espera passar o bounce da chave
+			_delay_ms(20);
+      
+      //incrementa variável do display
+			var++;
+
+      //atribue um valor máximo para a variável
+			if_value_max(99);
+
+      //quebra a variável em unit e dozen para o display 7 segmentos
+			convertVariable(var);
+
+      //enquanto a chave tiver precionada incrementa a variável
+			while(bit_is_clear(PIND, _chave01)) {
+        //testa quantas vezes a var foi incrementada sem ser solta
+				if(cont_tec_rapido < NUM_VEZES_LENTO) {	
+          //testa se já passo o tempo para incrmento lento	
+					if(cont_tec >= TEMPO_INCREMENTO_LENTO) {	
 						var++;					
 						if_value_max(99);
 						convertVariable(var);
-						contTecRapido++;					//incrementa a variável para mudança de lento para rápido
-						contTec = 0;						//zera variável do tempo do teclado
+
+            //incrementa a variável para mudança de lento para rápido
+						cont_tec_rapido++;
+
+            //zera variável do tempo do teclado
+						cont_tec = 0;
 					}
 				}
-				else{										//se passar o numer maximas de contagens lentas
-					if(contTec >= TEMPO_INCREMENTO_RAPIDO){	//testa se já passou o tempo para incremento rápido
+        //se passar o numer maximas de contagens lentas
+				else{
+          //testa se já passou o tempo para incremento rápido
+					if(cont_tec >= TEMPO_INCREMENTO_RAPIDO){
 						var++;
 						if_value_max(99);
 						convertVariable(var);
-						contTec = 0;						//zera variável do tempo do teclado
+
+            //zera variável do tempo do teclado
+						cont_tec = 0;
 					}
 				}
-				_delay_ms(1);								//espera um ms para as contagens de tempo entre os incrementos
-				contTec++;									//incrementa a variável que guarda o tempo passado para o incremento
+        //espera um ms para as contagens de tempo entre os incrementos
+				_delay_ms(1);
+
+        //incrementa a variável que guarda o tempo passado para o incremento
+				cont_tec++;
 			}
-			contTecRapido = 0;								//reinicia a variável para mudança de lento para rápido
-			contTec = 0;									//reinicia a variável para contar o tempo dos incremento do teclado	
+      //reinicia a variável para mudança de lento para rápido
+			cont_tec_rapido = 0;
+
+      //reinicia a variável para contar o tempo dos incremento do teclado
+			cont_tec = 0;
 		}
 		if (bit_is_clear(PIND, _chave02))
 		{
@@ -63,28 +88,38 @@ int main(void)
 			convertVariable(var);
 			while(bit_is_clear(PIND, _chave02))
 			{
-				if(contTecRapido < NUM_VEZES_LENTO){
-					if(contTec >= TEMPO_INCREMENTO_LENTO){
+				if(cont_tec_rapido < NUM_VEZES_LENTO){
+					if(cont_tec >= TEMPO_INCREMENTO_LENTO){
 						if_value_min(1);
 						var--;
 						convertVariable(var);
-						contTecRapido++;					//incrementa a variável para mudança de lento para rápido
-						contTec = 0;						//zera variável do tempo do teclado
+
+            //incrementa a variável para mudança de lento para rápido
+						cont_tec_rapido++;
+
+            //zera variável do tempo do teclado
+						cont_tec = 0;
 					}
 				}
 				else{
-					if(contTec >= TEMPO_INCREMENTO_RAPIDO){
+					if(cont_tec >= TEMPO_INCREMENTO_RAPIDO){
 						if_value_min(1);
 						var--;
 						convertVariable(var);
-						contTec = 0;						//zera variável do tempo do teclado
+            
+            //zera variável do tempo do teclado
+						cont_tec = 0;
 					}
 				}
 				_delay_ms(1);
-				contTec++;
+				cont_tec++;
 			}
-			contTecRapido = 0;								//reinicia a variável para mudança de lento para rápido
-			contTec = 0;
+
+      //reinicia a variável para mudança de lento para rápido
+			cont_tec_rapido = 0;
+      
+      //reinicia a variável para contar o tempo dos incremento do teclado
+			cont_tec = 0;
 		}
 	}
 }
